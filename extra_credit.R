5dafaff70890f35ec724de569400da18a1d68923
Chun Liu <cl316@saxon.stat.duke.edu>
2015-09-26 09:56:45 -0400

    added extra_credit.R

---
+++ extra_credit.R
@@ -0,0 +1,52 @@
+edge <- function(g){
+  # function where input is 'g' #
+  output <- data.frame(start = numeric(), end = numeric(), weight = numeric(),stringsAsFactors =F)
+  # makes an empty dataframe. where the horizontal header has "start", "end", and "weight" # 
+  # numeric() creates a zero vector for each column #
+  for(i in 1:length(g)){
+    cur <- g[i]
+    curvec <- unlist(cur)
+    num_to <- length(curvec)/2 
+    #number of vertices the current vertex goes to #
+    if(num_to==0)
+      next
+    for(j in 1:num_to){
+      newedge <- c(i,curvec[j],curvec[j+num_to])
+      output <- rbind(output,setNames(as.list(newedge), names(output)))
+    }
+    #convert numbers to letters
+    for(m in 1:length(output$start)){
+      for(n in 1:length(g)){
+        if(output$start[m] == n)
+          output$start[m] = replace(output$start[m],,names(g[n]))
+        if(output$end[m] == n)
+          output$end[m] = replace(output$end[m],,names(g[n]))
+      } 
+    }
+  }
+  return(output)
+}
+
+#min_span_tree function
+min_span_tree <- function(g){
+  
+  
+  edges <- edge(g)
+  #Remove redundancies for the edge table of an Undirected graph
+  edges <- edges[edges$start < edges$end,]
+  #The following code will apply Prim's Algorithm
+  edges <- edges[order(edges$weight,edges$start,edges$end),]
+  MST <- edges[1,]
+  vertices <- c(MST$start,MST$end) #record all the vertices in MST
+  while(nrow(MST) < length(g) - 1){
+    neighbor <- edges[(edges$start %in% vertices | edges$end %in% vertices) & (!(edges$start %in% vertices) | !(edges$end %in% vertices)),]
+    #neighbor <- diff_df(neigbor,MST)
+    #protection for non-connected components
+    if(nrow(neighbor) == 0){
+      stop("The input graph is not connected.")
+    }
+    MST <- rbind(MST,setNames(as.list(neighbor[1,]), names(MST)))
+    vertices <- c(vertices,MST$start,MST$end)
+  }
+  return(MST)
+}
